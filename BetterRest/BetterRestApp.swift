bbba35af2ffe1a478510dd4d247b57f489e5c2a2
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-10-14 22:59:40 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sofia Rodriguez Morales on 10/14/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
